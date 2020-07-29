21f5db945be894d7119a281953e284603a6098c5
CrAZYGreenAlpaca <39609875+CrAZYGreenAlpaca@users.noreply.github.com>
2020-07-28 23:07:07 -0700

    loss stuff

--- Python code/Loss_acc_rec_pres.py
+++ Python code/Loss_acc_rec_pres.py
@@ -1,6 +1,7 @@
 from boxcomparing import compute_iou
 import numpy as np
-
+from Model import compute_detections
+from boxcomparing import non_max_suppression
 
 def compute_recall(prediction_detections, truth_detections, threshold=0.5):
     ''' Compute the recall of predictions given targets.
@@ -74,3 +75,79 @@ def compute_precision(prediction_detections, truth_detections, threshold=0.5):
     total_preds = (predictions > 0).sum()
     return true_pos / total_preds if total_preds > 0 else 0
 
+def compute_ar(classifications, regressions, boxes, labels, nms_threshold=0.3, ar_threshold=0.5):
+    ''' Compute the average recall of the provided detections.
+    
+    Parameters
+    ----------
+    classifications : mygrad.Tensor, shape=(N, 12)
+        The classification predictions at each feature map location in the image.
+        
+    regressions : mygrad.Tensor, shape=(N, 12)
+        The regression predictions at each feature map location in the image.
+        
+    boxes : numpy.ndarray, shape=(K, 12)
+        The bounding boxes of the objects in the image, in (left, top, right, bottom) format.
+        
+    labels : numpy.ndarray, shape=(K, 1)
+        The labels of the objects in the image.
+        
+    nms_threshold : Real, optional (default=0.3)
+        The non-maximum suppression threshold to apply to the objects before computing ar.
+        
+    ar_threshold : Real, optional (default=0.5)
+        The threshold at which to count a predicted box as overlapping a ground-truth object.
+        
+    Returns
+    -------
+    float
+        The average precision for the given image.
+    '''
+
+
+    detections = compute_detections(classifications, regressions)
+    detections = detections[non_max_suppression(detections[:, :5], nms_threshold)]
+    detections = np.hstack((detections[:, :4], detections[:, -1:])) # remove score; keep label
+    
+    truth_detections = np.hstack((boxes, labels))
+    if len(detections) > 0:
+        return compute_recall(detections, truth_detections, ar_threshold)
+    return 0
+
+def compute_ap(classifications, regressions, boxes, labels, nms_threshold=0.3, ap_threshold=0.5):
+    ''' Compute the average precision of the provided detections.
+    
+    Parameters
+    ----------
+    classifications : mygrad.Tensor, shape=(N, 12)
+        The classification predictions at each feature map location in the image.
+        
+    regressions : mygrad.Tensor, shape=(N, 12)
+        The regression predictions at each feature map location in the image.
+        
+    boxes : numpy.ndarray, shape=(K, 12)
+        The bounding boxes of the objects in the image, in (left, top, right, bottom) format.
+        
+    labels : numpy.ndarray, shape=(K, 1)
+        The labels of the objects in the image.
+        
+    nms_threshold : Real, optional (default=0.3)
+        The non-maximum suppression threshold to apply to the objects before computing AP.
+        
+    ap_threshold : Real, optional (default=0.5)
+        The threshold at which to count a predicted box as overlapping a ground-truth object.
+        
+    Returns
+    -------
+    float
+        The average precision for the given image.
+    '''
+    
+    detections = compute_detections(classifications, regressions)
+    detections = detections[non_max_suppression(detections[:, :5], nms_threshold)]
+    detections = np.hstack((detections[:, :4], detections[:, -1:])) # remove score; keep label
+    
+    truth_detections = np.hstack((boxes, labels))
+    if len(detections) > 0:
+        return compute_precision(detections, truth_detections, ap_threshold)
+    return 0
